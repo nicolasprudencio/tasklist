0460c7dc137b775eb2c42713b4a32b6c2322f073
nicolasprudencio <nicolas-prudencio@hotmail.com>
2022-04-13 00:18:44 -0300

    concluded

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -10,20 +10,50 @@ interface Task {
   isComplete: boolean;
 }
 
+
+
+
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-  }
+    const taskId = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+  
+
+     if (!newTaskTitle) {
+      return alert('Adicione um título a sua task')
+    } else {
+      setTasks(tasks => ([...tasks, taskId])),
+      setNewTaskTitle('')
+  } 
+}
+
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskIsComplete = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }: task);
+
+    setTasks(taskIsComplete)
+    
+    
+    
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter(task => task.id !== id)
+
+    setTasks(removeTask)
   }
 
   return (
